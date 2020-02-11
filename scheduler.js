528ae56db4460df208453f214ff59758013cfc52
meg-twilio <mcummings@twilio.com>
2020-02-10 16:01:05 -0800

    initial setup

---
+++ scheduler.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const CronJob = require('cron').CronJob;
+const notificationsWorker = require('./workers/notificationsWorker');
+const moment = require('moment');
+
+const schedulerFactory = function() {
+    return {
+        start: function() {
+            new CronJob('00 * * * * *', function() {
+                console.log('Running Send Notifications Woker for ' + moment().format());
+                notificationsWorker.run();
+            }, null, true, '');
+        },
+    };
+};
+
+module.exports = schedulerFactory();
