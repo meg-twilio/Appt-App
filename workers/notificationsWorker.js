4f29c50ab198659838d1c79c5e385f0132888e03
meg-twilio <mcummings@twilio.com>
2020-02-11 15:15:48 -0800

    notificationsWorker creation

---
+++ workers/notificationsWorker.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Appointment = require('../models/appointment');
+
+ const notificationWorkerFactory = function() {
+     return {
+         run: function() {
+             Appointment.sendNotifications();
+         }
+     };
+ };
+
+module.exports = notificationWorkerFactory();
+ 
